fe2d8b5fee894e35798d91f244e0d5d52750af43
vporta <vporta7@gmail.com>
2016-04-06 01:53:35 -0400

    set up get method

--- keys.js
+++ keys.js
@@ -1,8 +1,25 @@
-console.log(process.argv);
+// console.log(process.argv);
 
-var command = process.argv[2];
+// var command = process.argv[2];
 
-exports.twitterKeys = {
+
+
+// var client = new twitter({
+//   consumer_key: 'MD6IhRgNxCLp4iLu4kXbM24yi',
+//   consumer_secret: 'aISS2Hb9Kpe3RDw3cu29q8I0pOJKQJqmjGxaI94gnTWDTiBnsD',
+//   access_token_key: '4910512006-5T0Gg5jGfLMqvBUfaANmgri13x1gmZIu7yTvt08',
+//   access_token_secret: 'ovrKvqSOpodrrjLVR9IB8aI4bDWADMfR9Ry3PLHONGzEq'
+// });
+
+// var params = {screen_name: 'vinjporta'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response){
+//   if (!error) {
+//     console.log(tweets);
+//   }
+// });
+
+
+module.exports = {
   consumer_key: 'MD6IhRgNxCLp4iLu4kXbM24yi',
   consumer_secret: 'aISS2Hb9Kpe3RDw3cu29q8I0pOJKQJqmjGxaI94gnTWDTiBnsD',
   access_token_key: '4910512006-5T0Gg5jGfLMqvBUfaANmgri13x1gmZIu7yTvt08',
@@ -11,39 +28,16 @@ exports.twitterKeys = {
 
 
 
-if(command === "my-tweets"){
-  console.log();
-}else if(command === "spotify-this-song"){
-  console.log();
-}else if(command === "movie-this"){
-  console.log();
-}else if(command === "do-what-it-says"){
-  console.log();
-}
 
-var baseURL = "https://userstream.twitter.com/1.1/user.json";
-var flickrApiUrl2 = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + key + "&tags="+flickrTerms+"&per_page=10&page=1&format=json&nojsoncallback=1"
-        
-        // console.log(typeof flickrTerms);
-            $.ajax({
-                url: flickrApiUrl2,
-                method: "GET",
-                // data: request,
-                // dataType: "json",
-                json: true,
-                success: function(flickrData){
-                    console.log(flickrData);
-                    var i;
-                    for(var i=0; i<9; i++){
 
-                    var photosFind = flickrData.photos.photo[i];
-                    var photosFindUrl = 'https://farm'+ photosFind.farm + '.staticflickr.com/' + photosFind.server + '/' + photosFind.id + '_' + photosFind.secret +'.jpg';
-                    var photoZoom = $('<a href="' + photosFindUrl + '">').addClass('fancybox');
-                    var photoElem = $('<img src="' + photosFindUrl + '">').addClass('flickrImages');
-   
-                    $('#flickr-health').append(photoElem);
-                    $('#flickr-health').append(photoZoom); 
-                    photoZoom.append(photoElem);
+// if(command === "my-tweets"){
+//   console.log();
+// }else if(command === "spotify-this-song"){
+//   console.log();
+// }else if(command === "movie-this"){
+//   console.log();
+// }else if(command === "do-what-it-says"){
+//   console.log();
+// }
 
-                    };
-                },
+
--- liri.js
+++ liri.js
@@ -1,4 +1,24 @@
-var someVar = require('./keys.js');
-// console.log(someVar.twitterKeys);
-// console.log(process.argv);
+console.log("this is loaded");
+var Twitter = require('twitter');
+var keys = require('./keys.js');
+var twit = new Twitter(keys);
+
+var params = { 
+  "id": 4910512006,
+  "id-str": "4910512006",
+  "count": 20
+}
+
+twit.get('search/tweets', params, gotData);
+
+function gotData(error, tweets, response) {
+  console.log(tweets);
+  var tweetData = tweets.statuses;
+  for(var i = 0; i < tweetData.length; i++) {
+    console.log(tweetData[i].text);
+  }
+}
+  
+
+
 
