7a726d9240eb78067c5df1c4f8af58902cb4a128
vporta <vporta7@gmail.com>
2016-04-08 03:08:27 -0400

    spotify api is working

--- liri.js
+++ liri.js
@@ -1,30 +1,38 @@
 console.log("LIRI node-bot is locked and loaded");
+// console.log(process.argv);
 var argument = process.argv[2];
-//Twitter bot begins
+var uri = process.argv[2];     
+var spotify = require('spotify');
+//Twitter variables
 var Twitter = require('twitter');
 var keys = require('./keys.js');
 var twit = new Twitter(keys);
-
 var params = { 
   "screen_name": "vinjporta",
   "count": 20
 }
-
 if(argument === "my-tweets"){
   twit.get('statuses/user_timeline', params, gotData);
   function gotData(error, data, response) {
-    var tweets = data;
+    var tweets = data; //data is the object
     for(var i = 0; i < tweets.length; i++) {
-      console.log(tweets[i].text);
-      console.log(tweets[i].created_at);
+      console.log(tweets[i].text); 
+      console.log(tweets[i].created_at); 
     }
   };
-}else {
-  console.log("nothing");
 }
-//Twitter bot ends 
+// Spotify bot begins 
+ spotify.search({ type: 'track', q: uri }, function(err, data) {
+ 
+    // Do something with 'data' 
+    console.log(data);
+});
 
-// 
+// else if(argument === "movie-this") {
+
+// }else if(argument === "do-what-it-says") {
+
+// }
 
 
   
