c1a28f77d91ed1d0af05f57ba812300d2eaf06c8
vporta <vporta7@gmail.com>
2016-04-08 00:07:30 -0400

    my-tweets argument works

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
-console.log("this is loaded");
-// console.log(process.argv);
+console.log("LIRI node-bot is locked and loaded");
+var argument = process.argv[2];
+//Twitter bot begins
 var Twitter = require('twitter');
 var keys = require('./keys.js');
 var twit = new Twitter(keys);
@@ -9,14 +10,23 @@ var params = {
   "count": 20
 }
 
-twit.get('statuses/user_timeline', params, gotData);
-
-function gotData(error, data, response) {
-  var tweets = data;
-  for(var i = 0; i < tweets.length; i++) {
-    console.log(tweets[i].text);
-  }
+if(argument === "my-tweets"){
+  twit.get('statuses/user_timeline', params, gotData);
+  function gotData(error, data, response) {
+    var tweets = data;
+    for(var i = 0; i < tweets.length; i++) {
+      console.log(tweets[i].text);
+      console.log(tweets[i].created_at);
+    }
+  };
+}else {
+  console.log("nothing");
 }
+//Twitter bot ends 
+
+// 
+
+
   
 
 
