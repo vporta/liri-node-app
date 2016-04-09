010ec8f92fb30805c6bbeb00983a5062f6a72f6c
vporta <vporta7@gmail.com>
2016-04-09 13:29:11 -0400

    spotify api commands work

--- liri.js
+++ liri.js
@@ -6,7 +6,7 @@ var argument = process.argv[2];
 var request = require('request');
 var fs = require('fs');
 var spotify = require('spotify');
-console.log(spotify);
+// console.log(spotify);
 var Twitter = require('twitter');
 var keys = require('./keys.js');
 // var ombd = require('./omdb.js');
@@ -24,24 +24,33 @@ if(argument === "my-tweets"){
       console.log(tweets[i].created_at); 
     }
   };
-}
-// Spotify bot begins 
-// else if(argument === "spotify-this-song") {
-//     var params = {
-//         'type': 'track',
-//         'q': uri   
-//     }
-//     spotify.search
-
-// }
-
-else if(argument === "movie-this") {
+}else if(argument === "movie-this") {
+    console.log(process.argv);
     var movieTitle = process.argv[3];
+    //add it into url t=
     request('http://www.omdbapi.com/?t=The+Notebook&y=&plot=short&r=json', function (error, response, body) {
       if (!error && response.statusCode == 200) {
         console.log(body); // Show the HTML for the Google homepage. 
       }
     })
+}
+// Spotify bot begins 
+else if(argument === "spotify-this-song") {
+    var songTitle = process.argv[3];
+    //add it into query
+    spotify.search({ type: 'track', query: songTitle }, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        // Do something with 'data' 
+        var data = data.tracks.items;
+        for(var i =0; i < data.length; i++){
+
+        console.log(data.items[i]);
+        }
+    });
+
 }
 
     // else if(argument === "do-what-it-says") {
